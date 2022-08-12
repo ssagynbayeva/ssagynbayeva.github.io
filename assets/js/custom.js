83a828d0ee518e030c8302579a1859d3acf05c3a
Sabina-star <83788641+Sabina-star@users.noreply.github.com>
2022-08-12 16:33:28 -0400

    preloader

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -46,3 +46,15 @@
 	});
 
 }(jQuery));
+
+/*-------------------------------------------------------------------------------
+  PRE LOADER
+-------------------------------------------------------------------------------*/
+
+$(window).load(function() {
+  $('.preloader').fadeOut(1000);  // set duration in brackets
+});
+
+$(document).ready(function() {
+  $('[data-toggle="tooltip"]').tooltip({boundary: 'window'});
+});
