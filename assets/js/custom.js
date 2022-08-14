e1c43db8796b3a474c38c00a68c910a6fb16ce78
Sabina-star <83788641+Sabina-star@users.noreply.github.com>
2022-08-13 22:40:38 -0400

    loader

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -69,31 +69,9 @@
 	// 	// Animate loader off screen
 	// 	$(".loader").fadeOut("slow");;
 	// });
-	/*-------------------------------------------------------------------------------
-  PRE LOADER
--------------------------------------------------------------------------------*/
-
-	$(window).load(function() {
-	  $('.loader').fadeOut(1000);  // set duration in brackets
-	});
-
-	$(document).ready(function() {
-	  $('[data-toggle="tooltip"]').tooltip({boundary: 'window'});
-	});
-
 
 	});
 
 }(jQuery));
 
-/*-------------------------------------------------------------------------------
-  PRE LOADER
--------------------------------------------------------------------------------*/
 
-// $(window).load(function() {
-//   $('.preloader').fadeOut(1000);  // set duration in brackets
-// });
-
-// $(document).ready(function() {
-//   $('[data-toggle="tooltip"]').tooltip({boundary: 'window'});
-// });
